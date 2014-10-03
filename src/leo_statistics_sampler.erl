6c04b1400a38051cd59582c20faed031dd767047
Yosuke Hara <yousuke.hara@gmail.com>
2014-10-03 17:36:03 +0900

    Fix leo-project/leofs/issues/250

--- src/leo_statistics_sampler.erl
+++ src/leo_statistics_sampler.erl
@@ -93,7 +93,11 @@ handle_info(timeout, State=#state{mod = Mod,
                                   timeout = Timeout}) ->
     timer:sleep(erlang:phash2(leo_date:clock(), 250)),
     catch Mod:handle_notify(),
+    {noreply, State, Timeout};
+
+handle_info(_, State=#state{timeout = Timeout}) ->
     {noreply, State, Timeout}.
+
 
 %% @doc This function is called by a gen_server when it is about to
 %%      terminate. It should be the opposite of Module:init/1 and do any necessary
